424aa4c71296e86c68175af762d91a1b2ed39b27
blighted <216200173+seaned1@users.noreply.github.com>
2025-10-26 23:47:08 +0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,11 +11,11 @@ const scale = 2;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 100) {
+    if (rank > 150) {
         return 0;
     }
 
-    if (rank > 100 && percent < 100) {
+    if (rank > 150 && percent < 100) {
         return 0;
     }
 
